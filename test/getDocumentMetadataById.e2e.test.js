29a541c355aacfb71c975ef428a9ef89d515b645
Seth Orell <seth.orell@gmail.com>
2024-03-23 12:27:09 -0500

    Open endpoint

--- test/getDocumentMetadataById.e2e.test.js
+++ test/getDocumentMetadataById.e2e.test.js
@@ -1,3 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import axios from 'axios';
+
 describe('When getting document metadata by id through API', () => {
-  it.todo('should return a 200');
+  it('should return a 200', async () => {
+    // ARRANGE
+    const axiosClient = axios.create();
+    console.log('region:', process.env.AWS_REGION);
+    const axiosOptions = {
+      baseURL: process.env.API_URL,
+      headers: {
+        'x-company-id': 'xyz',
+      },
+      validateStatus: () => true,
+    };
+    console.log('axiosOptions:', axiosOptions);
+
+    // ACT
+    const response = await axiosClient.get('/document-metadata/abc', axiosOptions);
+
+    // ASSERT
+    expect(response.status).toBe(200);
+  });
 });
